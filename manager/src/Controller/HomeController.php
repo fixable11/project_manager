25c2ce5b443424ca7b09d6d758ec1e5beb043118
fixable11 <fixable11@gmail.com>
2019-11-23 13:33:07 +0200

    Added pgsql

---
+++ manager/src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Index method
+     *
+     * @Route("/", name="home")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        phpinfo();
+        return $this->render('app/home.html.twig');
+    }
+}
