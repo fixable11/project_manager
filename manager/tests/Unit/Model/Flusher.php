fdea13b49ef2bb0a2512cae69b050b76acd12083
fixable11 <fixable11@gmail.com>
2019-11-26 19:16:06 +0200

    Filled UserRepository

--- manager/tests/Unit/Model/Flusher.php
+++ manager/tests/Unit/Model/Flusher.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Model;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class Flusher
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function flush(): void
     {
-
+        $this->em->flush();
     }
 }
